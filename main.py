ed723fc4b829df82b8f02827ee778aba2d454f67
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2023-08-16 20:11:17 +0100

    adds printed reporting of words/letters

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ def main():
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     print(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print()
+    print(f"{num_words} words found in the document")
+    print()
+    print("Distribution of letters:")
+    for item in get_sorted_chars_list(chars_dict):
+        print(f"{item[1]} of {item[0]}'s")
+    print()
+    print("--- End report ---")
+    get_sorted_chars_list(chars_dict)
 
 def get_num_words(text):
     words = text.split()
@@ -22,5 +32,14 @@ def get_chars_dict(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_sorted_chars_list(chars_dict):
+    list = chars_dict.items()
+    chars_list = []
+    for item in sorted(list, key=lambda a: a[1], reverse=True):
+        if item[0].isalpha():
+            chars_list.append(item)
+    return chars_list
+
 
 main()
